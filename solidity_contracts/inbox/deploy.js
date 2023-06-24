4f6a6c482d2b3c25fabc23b7c007fa1ecbd0cb5a
Mubashir Shariq <mubashirsharik01@gmail.com>
2023-06-24 15:42:27 +0530

    f

---
+++ solidity_contracts/inbox/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'fury wrestle knock chat old maximum borrow regular aunt aspect weather gown',
+  // remember to change this to your own phrase!
+  'https://sepolia.infura.io/v3/fb6504445b874aa2a731030951f9e890'
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
